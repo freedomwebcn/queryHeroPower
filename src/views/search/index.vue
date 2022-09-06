4293e71fcc16c9b39cdc7c4b8f378fedc3dbb988
freedomwebcn <freedomweb@yeah.net>
2022-09-06 14:29:25 +0800

    修改了search样式

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -11,19 +11,19 @@
         <span>分数</span>
       </div>
       <template v-for="(item, index) in searchData" :key="item.name">
-        <div :class="`service${index + 1} animate__animated animate__fadeInLeft ${index === searchData.length - 1 ? '' : 'van-hairline--bottom'}`">
+        <div :class="`hero-power${index + 1} animate__animated animate__fadeInLeft ${index === searchData.length - 1 ? '' : 'van-hairline--bottom'}`">
           <template v-for="(type, tyIndex) in dctype" :key="type">
-            <span class="service1-span" v-if="index == tyIndex">{{ type }}</span>
+            <span class="dctype" v-if="index == tyIndex">{{ type }}</span>
           </template>
-          <div class="service1-div">
+          <div class="district-score">
             <span>{{ item.province }}</span>
             <span>{{ item.provincePower }}</span>
           </div>
-          <div class="service1-div">
+          <div class="district-score">
             <span>{{ item.city }}</span>
             <span>{{ item.cityPower }}</span>
           </div>
-          <div class="service1-div">
+          <div class="district-score">
             <span>{{ item.area }}</span>
             <span>{{ item.areaPower }}</span>
           </div>
@@ -51,6 +51,7 @@ const errStatus = ref(null);
 
 async function getSearchData() {
   try {
+    errStatus.value = null;
     const data = await Promise.all([reqHeroPowerAqq({ heroName }), reqHeroPowerAwx({ heroName }), reqHeroPowerIqq({ heroName }), reqHeroPowerIwx({ heroName })]);
     searchData.value = data;
   } catch (error) {
@@ -96,12 +97,12 @@ getSearchData();
     .search-title {
       display: grid;
       grid-column: 1 / 4;
-      align-items: center;
+    //   align-items: center;
       grid-template-columns: 1fr 1fr 1fr;
       font-size: 16px;
     }
 
-    .service1 {
+    .hero-power1 {
       display: grid;
       grid-template-columns: 1fr 1fr 1fr;
       grid-template-rows: repeat(3, 1fr);
@@ -109,12 +110,12 @@ getSearchData();
       grid-row: 2 / span 6;
       font-size: 15px;
 
-      .service1-span {
+      .dctype {
         align-self: center;
         grid-row: 1 / span 3;
       }
 
-      .service1-div {
+      .district-score {
         display: grid;
         grid-template-columns: 1fr 1fr;
         grid-template-rows: 1fr;
@@ -123,18 +124,18 @@ getSearchData();
       }
     }
 
-    .service2 {
-      .service1();
+    .hero-power2 {
+      .hero-power1();
       grid-row: 8 / span 6;
     }
 
-    .service3 {
-      .service1();
+    .hero-power3 {
+      .hero-power1();
       grid-row: 14 / span 6;
     }
 
-    .service4 {
-      .service1();
+    .hero-power4 {
+      .hero-power1();
       grid-row: 20 / span 6;
     }
   }
